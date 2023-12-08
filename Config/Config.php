89b8b4ce901851129c767229ca906e1bbfb9b257
RodolfoCC10 <72413648+RodolfoCC10@users.noreply.github.com>
2023-12-07 21:17:10 -0600

    Add files via upload

---
+++ Config/Config.php
@@ -0,0 +1,11 @@
+<?php
+const BASE_URL = "http://localhost/shop/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "shop";
+const CHARSET = "charset=utf8";
+const TITLE = "SHOP";
+const MONEDA = "MXN";
+const CLIENT_ID = "ARiwTpC2jHS3vJmpMsD_-UPdgsJCAQfUbNRb5gjWQZH3hqvvnyOs2ZchmcunmlBqVSvUyNdT6x_G5BnX";
+?>
